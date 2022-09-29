0d48a9c64c1c63528745e875ed22b1a31f43922a
Kristina <ajiaha@gmail.com>
2022-09-29 21:19:45 +0300

    0.0

--- app/src/main/java/ru/krisproject/myapplication/MainActivity2.java
+++ app/src/main/java/ru/krisproject/myapplication/MainActivity2.java
@@ -2,7 +2,12 @@ package ru.krisproject.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.view.View;
+
+import java.security.PublicKey;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void goBack(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
