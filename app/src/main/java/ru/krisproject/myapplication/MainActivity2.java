141d4cb8a333b67710244ba9ef667737b7406a73
Kristina <ajiaha@gmail.com>
2022-09-30 19:18:42 +0300

    0.0

--- app/src/main/java/ru/krisproject/myapplication/MainActivity2.java
+++ app/src/main/java/ru/krisproject/myapplication/MainActivity2.java
@@ -1,24 +1,73 @@
 package ru.krisproject.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.security.PublicKey;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button btn_second;
+    TextView main_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn_second = findViewById(R.id.btn_second);
+        main_text = findViewById(R.id.main_text);
+        btn_second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo(main_text.getText().toString(), btn_second);
+            showInfoAlert("Вы хотите закрыть приложение?");
+            }
+        });
     }
 
     public void goBack(View v){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void btnClick(View v){
+        showInfo(((Button)v).getText().toString(), ((Button)v));
+    }
+
+    private void showInfoAlert(String text){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+        builder.setTitle("Большая подсказка").setMessage(text).setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showInfo(String text, Button btn){
+        btn.setText("Уже нажали");
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
 }
